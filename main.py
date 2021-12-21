0740e39ceb9e436ac2f2f79ed5cc905979189a92
Adrian Wasielewski <adrian.wasielewski@ilabo.com>
2021-12-21 14:23:30 +0100

    Catching event works

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3.10
 
 import asyncio
+from asyncio.events import AbstractEventLoop
+from asyncio.tasks import Task
+from time import sleep
 import socketio
 
 from authorization.jwtHandler import JwtHandler
@@ -12,22 +15,41 @@ sio = socketio.AsyncClient(
     reconnection_delay_max=5,
     randomization_factor=0.5,)
 
-@sio.on('check_remotes', namespace='remotes_control')
-async def check_remotes():
-    print('check remotes')
+checkRemotesTask: Task
+voteLoopTask: Task
 
-@sio.on('start', namespace='remotes_control')
-async def start():
-    print('message received with')
-    ##await sio.emit('my response', {'response': 'my response'})
+async def srint():
+    count = 1
+    while True: 
+        count += 1
+        await asyncio.sleep(1)
+        print('WorkWorkWork', count)
 
-@sio.event('stop', namespace='remotes_control')
-async def stop():
-    print('disconnected from server')
+@sio.on('connect')
+async def connect():
+    print('Connection established')
+
+@sio.on('remotes_control')
+async def on_remotes_control(data):
+    global checkRemotesTask
+    if data == 'start_check_remotes':
+        checkRemotesTask = asyncio.create_task(srint())
+        return 'OK'
+    if data == 'stop_check_remotes':
+        checkRemotesTask.cancel()
+        return 'OK'
+
+@sio.on('*')
+async def all(sid, data):
+    print(sid)
+    print(data)
 
 async def main():
-    await sio.connect('https://staging.sesja.pl/socket.io/?auth='+ JwtHandler.Generate(), namespaces='remotes_control')
-    await sio.connect('https://staging.sesja.pl/socket.io/?auth='+ JwtHandler.Generate(), namespaces='remotes_response')
+    await sio.connect('https://staging.sesja.pl/socket.io/?auth='+ JwtHandler.Generate())
+    print("Url: ", sio.connection_url)
+    print(sio.namespaces)
+    
+    await sio.emit("connected")
     await sio.wait()
 
 if __name__ == '__main__':
