70375dda36de236c81d745a17036d1fea32ae506
Wasielewski Adrian <wasielewski.adrian@o2.pl>
2022-01-19 15:47:41 +0100

    Add windows and linux suppot to open certificate

--- authorization/jwtHandler.py
+++ authorization/jwtHandler.py
@@ -4,6 +4,9 @@ import os
 class JwtHandler(object):
     @staticmethod
     def Generate(location):
-        privateKey = open(os.path.abspath(os.path.dirname(__file__)) + "\privateKey.pem")
+        slash = "/"
+        if os.name == 'nt':
+            slash = "\\"
+        privateKey = open(os.path.abspath(os.path.dirname(__file__)) + slash + "privateKey.pem")
         encoded_jwt = jwt.encode({"location": location}, privateKey.read(), algorithm="RS256")
         return encoded_jwt
